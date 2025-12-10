e7acf5117709dd25494e431a5892a247727e3938
Tomas Pinos <tpinos@cisco.com>
2025-12-10 22:22:45 +0100

    09

--- src/main/kotlin/day09/Day09.kt
+++ src/main/kotlin/day09/Day09.kt
@@ -2,6 +2,8 @@ package day09
 
 import common.XY
 import common.resourceFile
+import day09.HorizontalCrossing.*
+import day09.HorizontalCrossing.SPANNING
 import java.util.*
 import kotlin.math.abs
 import kotlin.math.max
@@ -40,14 +42,22 @@ class Day09(val filename: String) {
 
         rectangles.sortByDescending { it.area }
 
-        val edgesAndBorders = edgesAndBorders(points)
+        val edges = edges(points)
+
+        val hEdgesByY = edges.horizontal.groupBy { it.from.y }
+        val vEdgesByX = edges.vertical.groupBy { it.from.x }
+
+        println(hEdgesByY.filter { it.value.size > 1 })
+        println(vEdgesByX.filter { it.value.size > 1 })
+
+        val edgesByY = edgesByY(edges)
 
         for (i in rectangles.indices) {
             val rectangle = rectangles[i]
-            if (i % 1 == 0) {
-                println("$i/${rectangles.size}: $rectangle")
+            if (i % 500 == 0) {
+                println("${i + 1}/${rectangles.size}: $rectangle")
             }
-            if (checkRectangle(rectangle, edgesAndBorders)) {
+            if (checkRectangle(rectangle, edgesByY)) {
                 return rectangle.area
             }
         }
@@ -56,109 +66,132 @@ class Day09(val filename: String) {
     }
 }
 
+fun checkRectangle(rectangle: Rectangle, edges: EdgesByY): Boolean {
+    val lines = rectangle.horizontalLines()
+    return lines.all {
+        val inside = checkLine(it, edges)
+        //if (!inside) println(it)
+        inside
+    }
+}
+
+fun checkLine(line: Edge, edges: EdgesByY): Boolean {
+    val y = line.from.y
+    return checkLine(Line(line.from.x, line.to.x), edges.horizontal[y], edges.vertical[y])
+}
+
+data class Line(val from: Int, val to: Int) {
+    fun isInsideHorizontally(other: Line): Boolean {
+        return from >= other.from && to <= other.to
+    }
+}
+
+fun checkLine(line: Line, horizontalEdge: Line?, verticalEdges: TreeSet<Int>?): Boolean {
+    if (horizontalEdge == null && verticalEdges == null) {
+        // outside of the polygon
+        return false
+    }
+    if (horizontalEdge != null && verticalEdges == null) {
+        // only horizontal edge
+        return line.isInsideHorizontally(horizontalEdge)
+    } else if (horizontalEdge == null && verticalEdges != null) {
+        // only vertical edges
+        return isInsideVerticals(line.from, line.to, verticalEdges)
+    } else {
+        val hCrossing = checkHorizontalCrossing(line, horizontalEdge!!)
+        when (hCrossing) {
+            INSIDE -> return true
+            OUTSIDE -> return isInsideVerticals(line.from, line.to, verticalEdges!!)
+            HEADING -> {
+                return verticalEdges!!.headSet(horizontalEdge.from, false).size % 2 == 1
+            }
+
+            TAILING -> {
+                return verticalEdges!!.tailSet(horizontalEdge.to, false).size % 2 == 1
+            }
+
+            SPANNING -> {
+                return verticalEdges!!.headSet(horizontalEdge.from, false).size % 2 == 1 &&
+                        verticalEdges.tailSet(horizontalEdge.to, false).size % 2 == 1
+            }
+        }
+    }
+
+    return false
+}
+
+fun isInsideVerticals(fromX: Int, toX: Int, verticalEdges: TreeSet<Int>): Boolean {
+    val verticalsBefore = verticalEdges.headSet(fromX, true)
+    if (verticalsBefore.size % 2 == 0) return false
+    val verticalsAfter = verticalEdges.tailSet(fromX, false)
+    if (verticalsAfter.isEmpty()) {
+        throw IllegalStateException("No vertical after: $fromX - $toX")
+    }
+    return verticalsAfter.first() >= toX
+}
+
+enum class HorizontalCrossing {
+    INSIDE, OUTSIDE, HEADING, TAILING, SPANNING
+}
+
+fun checkHorizontalCrossing(line: Line, horizontal: Line): HorizontalCrossing {
+    val horizontalRange = horizontal.from..horizontal.to
+    if (line.from in horizontalRange && line.to in horizontalRange) return INSIDE
+    else if (line.from < horizontal.from && line.to in horizontalRange) return HEADING
+    else if (line.from in horizontalRange && line.to > horizontal.to) return TAILING
+    else if (line.from < horizontal.from && line.to > horizontal.to) return SPANNING
+    else return OUTSIDE
+}
+
 data class Edge(val from: XY, val to: XY)
 
-data class EdgesOnY(val byFromX: TreeMap<Int, Edge>, val byToX: TreeMap<Int, Edge>)
+data class Edges(val horizontal: List<Edge>, val vertical: List<Edge>)
 
-data class HEdgesAndVBorders(val hEdges: Map<Int, EdgesOnY>, val vBorders: MutableMap<Int, TreeSet<Int>>)
+data class EdgesByY(val horizontal: Map<Int, Line>, val vertical: Map<Int, TreeSet<Int>>)
 
-fun edgesAndBorders(points: List<XY>): HEdgesAndVBorders {
-    val hEdges = mutableMapOf<Int, EdgesOnY>()
-    val vBorders = mutableMapOf<Int, TreeSet<Int>>()
-
+fun edges(points: List<XY>): Edges {
     val closedPoints = mutableListOf<XY>()
     closedPoints.addAll(points)
     closedPoints.add(points.first())
+
+    val horizontal = mutableListOf<Edge>()
+    val vertical = mutableListOf<Edge>()
 
     var from = closedPoints[0]
     for (i in 1..closedPoints.lastIndex) {
         val to = closedPoints[i]
         if (from.x == to.x) {
             // vertical edge
-            for (y in min(from.y, to.y) + 1..<max(from.y, to.y)) {
-                if (vBorders.containsKey(y)) {
-                    vBorders[y]!!.add(from.x)
-                } else {
-                    vBorders[y] = TreeSet<Int>().apply { add(from.x) }
-                }
-            }
-        } else {
+            vertical.add(Edge(XY(from.x, min(from.y, to.y)), XY(from.x, max(from.y, to.y))))
+        } else if (from.y == to.y) {
             // horizontal edge
-            if (!hEdges.contains(from.y)) {
-                hEdges[from.y] = EdgesOnY(TreeMap(), TreeMap())
-            }
-            val fromX = min(from.x, to.x)
-            val toX = max(from.x, to.x)
-            val edge = Edge(XY(fromX, from.y), XY(toX, from.y))
-            hEdges[from.y]!!.byFromX.put(fromX, edge)
-            hEdges[from.y]!!.byToX.put(toX, edge)
+            horizontal.add(Edge(XY(min(from.x, to.x), from.y), XY(max(from.x, to.x), from.y)))
+        } else {
+            throw IllegalStateException("Bad edge: $from -> $to")
         }
         from = to
     }
 
-    return HEdgesAndVBorders(hEdges, vBorders)
+    return Edges(horizontal, vertical)
 }
 
-fun checkRectangle(rectangle: Rectangle, edgesAndBorders: HEdgesAndVBorders): Boolean {
-//    println(rectangle)
-    val upLeft = rectangle.upLeft()
-    val downRight = rectangle.downRight()
-    for (y in upLeft.y..downRight.y) {
-        val hEdges = edgesAndBorders.hEdges.getOrElse(y) { EdgesOnY(TreeMap(), TreeMap()) }
-        val vBorders = edgesAndBorders.vBorders.getOrElse(y) { TreeSet() }
+fun edgesByY(edges: Edges): EdgesByY {
+    val horizontal = HashMap<Int, Line>()
+    val vertical = HashMap<Int, TreeSet<Int>>()
 
-        var x = upLeft.x
-        while (x <= downRight.x) {
-            println("$x,$y")
+    for (edge in edges.horizontal) {
+        if (horizontal.containsKey(edge.from.y)) throw IllegalStateException("Bad edge: $edge")
+        horizontal[edge.from.y] = Line(edge.from.x, edge.to.x)
+    }
 
-            // inside horizontal edge?
-            val edgeOnLeft = hEdges.byFromX.floorEntry(x)
-            val edgeOnRight = hEdges.byToX.ceilingEntry(x)
-            // the point is inside a horizontal edge
-            if (edgeOnLeft != null && edgeOnRight != null && edgeOnLeft.value == edgeOnRight.value) {
-                x = edgeOnRight.value!!.to.x + 1
-                continue
-            }
-
-            // inside vertical edge?
-            if (vBorders.contains(x)) {
-                x++
-                continue
-            }
-
-            val headBorders = vBorders.headSet(x)
-            if (headBorders.isNotEmpty()) {
-                // there is a vertical edge on the left
-                if (headBorders.size % 2 == 1) {
-                    // inside the polygon
-                    val tailBorders = vBorders.tailSet(x)
-                    check(tailBorders.isNotEmpty()) { "Lost inside the polygon" }
-                    x = tailBorders.first() + 1
-                    continue
-                } else {
-                    // outside the polygon
-                    return false
-                }
-            } else {
-                // there is no vertical edge on the left
-                // maybe there's a horizontal edge on the left
-                if (edgeOnLeft != null) {
-                    val yMinu1Inside = edgesAndBorders.vBorders.getOrElse(y - 1) { TreeSet() }.headSet(x).size % 2 == 1
-                    val yPlus1Inside = edgesAndBorders.vBorders.getOrElse(y + 1) { TreeSet() }.headSet(x).size % 2 == 1
-                    if (yMinu1Inside || yPlus1Inside) {
-                        x++
-                        continue
-                    } else {
-                        return false
-                    }
-                } else {
-                    return false
-                }
-            }
+    for (edge in edges.vertical) {
+        for (y in edge.from.y..edge.to.y) {
+            if (!vertical.containsKey(y)) vertical[y] = TreeSet()
+            vertical[y]!!.add(edge.from.x)
         }
     }
 
-    return true
+    return EdgesByY(horizontal, vertical)
 }
 
 data class Rectangle(
@@ -172,6 +205,16 @@ data class Rectangle(
 
     fun downRight(): XY {
         return XY(max(a.x, b.x), max(a.y, b.y))
+    }
+
+    fun horizontalLines(): List<Edge> {
+        val upLeft = upLeft()
+        val downRight = downRight()
+        val lines = mutableListOf<Edge>()
+        for (y in upLeft.y..downRight.y) {
+            lines.add(Edge(XY(upLeft.x, y), XY(downRight.x, y)))
+        }
+        return lines
     }
 }
 
--- src/test/kotlin/day09/Day09Test.kt
+++ src/test/kotlin/day09/Day09Test.kt
@@ -3,9 +3,6 @@ package day09
 import common.XY
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
-import java.util.*
-import kotlin.test.assertFalse
-import kotlin.test.assertTrue
 
 private const val INPUT = "/day09/input.txt"
 
@@ -25,43 +22,5 @@ class Day09Test {
     fun testReadInput() {
         val points = readInput(INPUT)
         assertEquals(XY(7, 1), points[0])
-    }
-
-    @Test
-    fun testEdgesAndBorders() {
-        val points = readInput(INPUT)
-        val (hEdges, vBorders) = edgesAndBorders(points)
-
-        assertTrue { hEdges[0] == null }
-        assertEquals(listOf(Edge(XY(7, 1), XY(11, 1))), hEdges[1])
-        assertTrue { hEdges[2] == null }
-        assertEquals(listOf(Edge(XY(2, 3), XY(7, 3))), hEdges[3])
-
-        assertTrue { vBorders[0] == null }
-
-        assertEquals(2, vBorders[1]!!.size)
-        assertEquals(listOf(7, 11), vBorders[1]!!.toList())
-
-        assertEquals(2, vBorders[2]!!.size)
-        assertEquals(listOf(7, 11), vBorders[2]!!.toList())
-
-        assertEquals(3, vBorders[3]!!.size)
-        assertEquals(listOf(2, 7, 11), vBorders[3]!!.toList())
-    }
-
-    @Test
-    fun testCheckX() {
-//        assertFalse { checkX(6, EdgesOnY(TreeMap(), TreeMap()), TreeSet(listOf(7, 11))) }
-//        assertTrue { checkX(7, EdgesOnY(TreeMap(), TreeMap()), TreeSet(listOf(7, 11))) }
-//        assertTrue { checkX(11, EdgesOnY(TreeMap(), TreeMap()), TreeSet(listOf(7, 11))) }
-//        assertFalse { checkX(12, listOf(), TreeSet(listOf(7, 11))) }
-//
-//        assertFalse { checkX(1, listOf(), TreeSet(listOf(2, 7, 11))) }
-//        assertTrue { checkX(2, listOf(), TreeSet(listOf(2, 7, 11))) }
-//        assertTrue { checkX(7, listOf(), TreeSet(listOf(2, 7, 11))) }
-//        assertTrue { checkX(8, listOf(), TreeSet(listOf(2, 7, 11))) }
-//        assertTrue { checkX(10, listOf(), TreeSet(listOf(2, 7, 11))) }
-//        assertTrue { checkX(11, listOf(), TreeSet(listOf(2, 7, 11))) }
-//        assertFalse { checkX(12, listOf(), TreeSet(listOf(2, 7, 11))) }
     }
 }
