ef6b3214e14d92db283cc2f1249210f93133dd58
Tomas Pinos <tpinos@splunk.com>
2025-12-11 10:07:13 +0100

    09

--- src/main/kotlin/day09/Day09.kt
+++ src/main/kotlin/day09/Day09.kt
@@ -3,7 +3,6 @@ package day09
 import common.XY
 import common.resourceFile
 import day09.HorizontalCrossing.*
-import day09.HorizontalCrossing.SPANNING
 import java.util.*
 import kotlin.math.abs
 import kotlin.math.max
@@ -11,7 +10,7 @@ import kotlin.math.min
 
 fun main() {
     val day = Day09("/day09/input.txt")
-    //println(day.part1())
+    println(day.part1())
     println(day.part2())
 }
 
@@ -45,102 +44,26 @@ class Day09(val filename: String) {
         val edges = edges(points)
 
         val hEdgesByY = edges.horizontal.groupBy { it.from.y }
-        val vEdgesByX = edges.vertical.groupBy { it.from.x }
-
-        println(hEdgesByY.filter { it.value.size > 1 })
-        println(vEdgesByX.filter { it.value.size > 1 })
+        val twoHEdgesOnTheSameY = hEdgesByY.filter { it.value.size > 1 }.isNotEmpty()
+        if (twoHEdgesOnTheSameY) error("Two horizontal edges on the same Y")
 
         val edgesByY = edgesByY(edges)
 
         for (i in rectangles.indices) {
             val rectangle = rectangles[i]
-            if (i % 500 == 0) {
-                println("${i + 1}/${rectangles.size}: $rectangle")
-            }
-            if (checkRectangle(rectangle, edgesByY)) {
-                return rectangle.area
-            }
+            if (checkRectangle(rectangle, edgesByY)) return rectangle.area
         }
 
         return -1
     }
 }
 
-fun checkRectangle(rectangle: Rectangle, edges: EdgesByY): Boolean {
-    val lines = rectangle.horizontalLines()
-    return lines.all {
-        val inside = checkLine(it, edges)
-        //if (!inside) println(it)
-        inside
-    }
-}
-
-fun checkLine(line: Edge, edges: EdgesByY): Boolean {
-    val y = line.from.y
-    return checkLine(Line(line.from.x, line.to.x), edges.horizontal[y], edges.vertical[y])
+enum class HorizontalCrossing {
+    INSIDE, OUTSIDE_LEFT, OUTSIDE_RIGHT, HEADING, TAILING, SPANNING
 }
 
 data class Line(val from: Int, val to: Int) {
-    fun isInsideHorizontally(other: Line): Boolean {
-        return from >= other.from && to <= other.to
-    }
-}
-
-fun checkLine(line: Line, horizontalEdge: Line?, verticalEdges: TreeSet<Int>?): Boolean {
-    if (horizontalEdge == null && verticalEdges == null) {
-        // outside of the polygon
-        return false
-    }
-    if (horizontalEdge != null && verticalEdges == null) {
-        // only horizontal edge
-        return line.isInsideHorizontally(horizontalEdge)
-    } else if (horizontalEdge == null && verticalEdges != null) {
-        // only vertical edges
-        return isInsideVerticals(line.from, line.to, verticalEdges)
-    } else {
-        val hCrossing = checkHorizontalCrossing(line, horizontalEdge!!)
-        when (hCrossing) {
-            INSIDE -> return true
-            OUTSIDE -> return isInsideVerticals(line.from, line.to, verticalEdges!!)
-            HEADING -> {
-                return verticalEdges!!.headSet(horizontalEdge.from, false).size % 2 == 1
-            }
-
-            TAILING -> {
-                return verticalEdges!!.tailSet(horizontalEdge.to, false).size % 2 == 1
-            }
-
-            SPANNING -> {
-                return verticalEdges!!.headSet(horizontalEdge.from, false).size % 2 == 1 &&
-                        verticalEdges.tailSet(horizontalEdge.to, false).size % 2 == 1
-            }
-        }
-    }
-
-    return false
-}
-
-fun isInsideVerticals(fromX: Int, toX: Int, verticalEdges: TreeSet<Int>): Boolean {
-    val verticalsBefore = verticalEdges.headSet(fromX, true)
-    if (verticalsBefore.size % 2 == 0) return false
-    val verticalsAfter = verticalEdges.tailSet(fromX, false)
-    if (verticalsAfter.isEmpty()) {
-        throw IllegalStateException("No vertical after: $fromX - $toX")
-    }
-    return verticalsAfter.first() >= toX
-}
-
-enum class HorizontalCrossing {
-    INSIDE, OUTSIDE, HEADING, TAILING, SPANNING
-}
-
-fun checkHorizontalCrossing(line: Line, horizontal: Line): HorizontalCrossing {
-    val horizontalRange = horizontal.from..horizontal.to
-    if (line.from in horizontalRange && line.to in horizontalRange) return INSIDE
-    else if (line.from < horizontal.from && line.to in horizontalRange) return HEADING
-    else if (line.from in horizontalRange && line.to > horizontal.to) return TAILING
-    else if (line.from < horizontal.from && line.to > horizontal.to) return SPANNING
-    else return OUTSIDE
+    fun isInsideHorizontally(other: Line): Boolean = from >= other.from && to <= other.to
 }
 
 data class Edge(val from: XY, val to: XY)
@@ -148,51 +71,6 @@ data class Edge(val from: XY, val to: XY)
 data class Edges(val horizontal: List<Edge>, val vertical: List<Edge>)
 
 data class EdgesByY(val horizontal: Map<Int, Line>, val vertical: Map<Int, TreeSet<Int>>)
-
-fun edges(points: List<XY>): Edges {
-    val closedPoints = mutableListOf<XY>()
-    closedPoints.addAll(points)
-    closedPoints.add(points.first())
-
-    val horizontal = mutableListOf<Edge>()
-    val vertical = mutableListOf<Edge>()
-
-    var from = closedPoints[0]
-    for (i in 1..closedPoints.lastIndex) {
-        val to = closedPoints[i]
-        if (from.x == to.x) {
-            // vertical edge
-            vertical.add(Edge(XY(from.x, min(from.y, to.y)), XY(from.x, max(from.y, to.y))))
-        } else if (from.y == to.y) {
-            // horizontal edge
-            horizontal.add(Edge(XY(min(from.x, to.x), from.y), XY(max(from.x, to.x), from.y)))
-        } else {
-            throw IllegalStateException("Bad edge: $from -> $to")
-        }
-        from = to
-    }
-
-    return Edges(horizontal, vertical)
-}
-
-fun edgesByY(edges: Edges): EdgesByY {
-    val horizontal = HashMap<Int, Line>()
-    val vertical = HashMap<Int, TreeSet<Int>>()
-
-    for (edge in edges.horizontal) {
-        if (horizontal.containsKey(edge.from.y)) throw IllegalStateException("Bad edge: $edge")
-        horizontal[edge.from.y] = Line(edge.from.x, edge.to.x)
-    }
-
-    for (edge in edges.vertical) {
-        for (y in edge.from.y..edge.to.y) {
-            if (!vertical.containsKey(y)) vertical[y] = TreeSet()
-            vertical[y]!!.add(edge.from.x)
-        }
-    }
-
-    return EdgesByY(horizontal, vertical)
-}
 
 data class Rectangle(
     val a: XY,
@@ -216,6 +94,116 @@ data class Rectangle(
         }
         return lines
     }
+}
+
+fun checkRectangle(rectangle: Rectangle, edges: EdgesByY): Boolean {
+    val lines = rectangle.horizontalLines()
+    return lines.all { checkLine(it, edges) }
+}
+
+fun checkLine(line: Edge, edges: EdgesByY): Boolean {
+    val y = line.from.y
+    return checkLine(Line(line.from.x, line.to.x), edges.horizontal[y], edges.vertical[y])
+}
+
+fun checkLine(line: Line, horizontalEdge: Line?, verticalEdges: TreeSet<Int>?): Boolean {
+    if (horizontalEdge == null && verticalEdges == null) {
+        // outside the polygon
+        return false
+    }
+    if (horizontalEdge != null && verticalEdges == null) {
+        // only horizontal edge
+        return line.isInsideHorizontally(horizontalEdge)
+    } else if (horizontalEdge == null && verticalEdges != null) {
+        // only vertical edges
+        return isInsideVerticals(line.from, line.to, verticalEdges)
+    } else {
+        val hCrossing = checkHorizontalCrossing(line, horizontalEdge!!)
+        return when (hCrossing) {
+            INSIDE -> true
+            OUTSIDE_LEFT -> verticalEdges!!.headSet(line.from, true).size % 2 == 1
+            OUTSIDE_RIGHT -> verticalEdges!!.tailSet(line.to, true).size % 2 == 1
+            HEADING -> verticalEdges!!.headSet(horizontalEdge.from, true).size % 2 == 1
+            TAILING -> verticalEdges!!.tailSet(horizontalEdge.to, true).size % 2 == 1
+            SPANNING -> verticalEdges!!.headSet(horizontalEdge.from, true).size % 2 == 1 &&
+                    verticalEdges.tailSet(horizontalEdge.to, true).size % 2 == 1
+        }
+    }
+}
+
+fun isInsideVerticals(fromX: Int, toX: Int, verticalEdges: TreeSet<Int>): Boolean {
+    val verticalsBefore = verticalEdges.headSet(fromX, true)
+    // odd count expected
+    if (verticalsBefore.size % 2 == 0) return false
+    val verticalsAfter = verticalEdges.tailSet(fromX, false)
+    if (verticalsAfter.isEmpty()) {
+        error("No vertical after: $fromX - $toX")
+    }
+    return verticalsAfter.first() >= toX
+}
+
+fun checkHorizontalCrossing(line: Line, horizontal: Line): HorizontalCrossing {
+    val range = horizontal.from..horizontal.to
+    return when {
+        line.from in range && line.to in range -> INSIDE
+        line.from < horizontal.from && line.to in range -> HEADING
+        line.from in range && line.to > horizontal.to -> TAILING
+        line.from < horizontal.from && line.to > horizontal.to -> SPANNING
+        line.from < horizontal.from && line.to < horizontal.from -> OUTSIDE_LEFT
+        line.from > horizontal.to && line.to > horizontal.to -> OUTSIDE_RIGHT
+        else -> error("Bad horizontal crossing: $line x $horizontal")
+    }
+}
+
+fun edges(points: List<XY>): Edges {
+    val closedPoints = mutableListOf<XY>()
+    closedPoints.addAll(points)
+    closedPoints.add(points.first())
+
+    val horizontal = mutableListOf<Edge>()
+    val vertical = mutableListOf<Edge>()
+
+    var from = closedPoints[0]
+    for (i in 1..closedPoints.lastIndex) {
+        val to = closedPoints[i]
+        if (from.x == to.x) {
+            // vertical edge
+            val minY = min(from.y, to.y)
+            val maxY = max(from.y, to.y)
+            if (minY == maxY) error("Bad edge: $from -> $to")
+            if (maxY - minY > 1) {
+                // don't include beginning and end - they are part of horizontal edges
+                vertical.add(Edge(XY(from.x, minY + 1), XY(from.x, maxY - 1)))
+            }
+        } else if (from.y == to.y) {
+            // horizontal edge
+            horizontal.add(Edge(XY(min(from.x, to.x), from.y), XY(max(from.x, to.x), from.y)))
+        } else {
+            error("Bad edge: $from -> $to")
+        }
+        from = to
+    }
+
+    return Edges(horizontal, vertical)
+}
+
+fun edgesByY(edges: Edges): EdgesByY {
+    val horizontal = HashMap<Int, Line>()
+    val vertical = HashMap<Int, TreeSet<Int>>()
+
+    for (edge in edges.horizontal) {
+        if (horizontal.containsKey(edge.from.y)) error("Bad edge: $edge")
+        horizontal[edge.from.y] = Line(edge.from.x, edge.to.x)
+    }
+
+    for (edge in edges.vertical) {
+        for (y in edge.from.y..edge.to.y) {
+            if (!vertical.containsKey(y)) vertical[y] = TreeSet()
+            vertical[y]!!.add(edge.from.x)
+        }
+    }
+
+    return EdgesByY(horizontal, vertical)
 }
 
 fun readInput(filename: String): List<XY> {
